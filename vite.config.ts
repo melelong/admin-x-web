9d7e2484712f54bef3257100600e27f7c14c97e9
格子 <fl9420@qq.com>
2025-06-02 12:52:29 +0800

    chore: use unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,23 @@ import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
 import path from 'path';
 import eslint from '@nabla/vite-plugin-eslint';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      },
+    }),
     eslint({
       eslintOptions: {
         fix: true,
