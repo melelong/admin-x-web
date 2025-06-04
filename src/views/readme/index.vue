c6ea0c2f0b698cadbe18bfa5d3f62eb1af0b66c9
格子 <fl9420@qq.com>
2025-06-04 08:00:19 +0800

    feat: readme

--- src/views/readme/index.vue
+++ src/views/readme/index.vue
@@ -1,5 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
 
-<template></template>
+import MarkdownViewer from '@/components/MarkdownViewer/index.vue';
 
-<style scoped></style>
+const markdownContent = ref('');
+const isLoading = ref(true);
+
+const loadMarkdownFile = async () => {
+  try {
+    const response = await fetch('README.md');
+    markdownContent.value = await response.text();
+  } catch (error) {
+    markdownContent.value = '# 加载失败\n请检查文件路径';
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  loadMarkdownFile();
+});
+</script>
+
+<template>
+  <div class="m-10px">
+    <div class="p-16px bg-white rounded">
+      <a-spin :spinning="isLoading">
+        <MarkdownViewer :content="markdownContent" />
+      </a-spin>
+    </div>
+  </div>
+</template>
