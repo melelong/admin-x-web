895e81651219b6d4dcb53bcdee88cfd5b3df8f7f
格子 <fl9420@qq.com>
2025-06-08 19:35:44 +0800

    chore: 优化首页字体样式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 
 <template>
   <div class="m-10px">
     <div class="p-16px bg-white rounded">
-      <h1>首页</h1>
-      <p>欢迎使用 Admin X</p>
+      <div>
+        <h1>首页</h1>
+        <div class="mt-10px flex justify-between items-end font-size-16px">
+          <p>欢迎使用 Admin X</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
