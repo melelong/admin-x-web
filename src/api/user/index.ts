3aeed0382e727bb9b45ee6193fd39b8bcc5830f2
格子 <fl9420@qq.com>
2025-06-17 23:48:16 +0800

    feat: 用户查询

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,1 +1,22 @@
+import { PageResult } from '@/types/global';
+import request, { ResponseData } from '@/utils/request';
+
 export * from './auth';
+
+export interface User {
+  userId: number
+  deptId: number
+  username: string
+  nickname: string
+  email: string
+  phone: string
+  avatar: string
+  address: string
+  age: number
+  name: string
+  gender: number
+}
+
+export const listUser = (data: { pageNum: number, pageSize: number }): Promise<ResponseData<PageResult<User>>> => {
+  return request.get('/user/listUser', data);
+};
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -57,7 +57,7 @@ const handleReset = () => {
 };
 
 const handleSearch = () => {
-
+  getDataSource()
 };
 
 const roleFormModalRef = ref();
