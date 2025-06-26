69677b86fc7d404cdc4f9bb48d97147c18871d65
李江辰 <ljc.byte@gmail.com>
2025-06-26 23:03:32 +0800

    build: 资源压缩优化

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import AutoImport from 'unplugin-auto-import/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression';
+import imagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/' : '/',
@@ -29,12 +30,39 @@ export default defineConfig({
         }),
       ],
     }),
-    viteCompression({
+    compression({
       verbose: true,
       disable: false,
       threshold: 10240,
       algorithm: 'gzip',
       ext: '.gz',
+    }),
+    imagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
     }),
     eslint({
       eslintOptions: {
