007e0c499a6d16b4dc6232cee869cac35c4ef632
格子 <fl9420@qq.com>
2025-07-15 00:39:26 +0800

    chore: 优化首页加载

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { RouterView } from 'vue-router';
-import AppLoader from '@/components/AppLoader/index.vue';
 
 const { locale } = useI18n();
 const antLocale = computed(() => (locale.value === 'zh-CN' ? zhCN : enUS));
@@ -17,15 +16,25 @@ const { themeConfig } = storeToRefs(themeStore);
 const isAppLoaded = ref(false);
 
 onMounted(() => {
+  const appLoading = document.getElementById('app-loading-container');
+  if (appLoading) {
+    appLoading.style.transition = 'opacity 2048ms ease-in';
+    appLoading.style.opacity = '0';
+    setTimeout(() => {
+      appLoading.remove();
+    }, 2048);
+  }
+
   setTimeout(() => {
     isAppLoaded.value = true;
-  }, 900);
+  }, 520);
 });
 </script>
 
 <template>
-  <AppLoader v-if="!isAppLoaded" />
-  <a-config-provider v-show="isAppLoaded" :theme="themeConfig" :locale="antLocale">
-    <RouterView />
-  </a-config-provider>
+  <div v-show="isAppLoaded">
+    <a-config-provider :theme="themeConfig" :locale="antLocale">
+      <RouterView />
+    </a-config-provider>
+  </div>
 </template>
